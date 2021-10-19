0d90e0abd31665adf7b8e42cbd964feb2f410207
Yon Txpmdroflol <jochong27@gmail.com>
2021-10-19 16:55:07 +0200

    search

--- index.js
+++ index.js
@@ -1,20 +1,47 @@
-var personnage = document.getElementsByClassName('row')[0]
+var personnage = document.getElementsByClassName('personnage')[0]
 
 $(function() {
     $.ajax({
         url: 'https://thronesapi.com/api/v2/Characters',
         success: function(data, statuts, response) {
-            data.forEach(element => {
-                personnage.innerHTML = personnage.innerHTML + `
-                
-                    <div class="col-lg-4 col-md-4">
-                        <img src="${element.imageUrl}" alt="">
-                        <p>${element.firstName} ${element.lastName}</p>
-                        <p>${element.title}</p>
-                    </div>
-                
-                `
-            })
+            afficherlist(data)
         }
     })
 })
+
+function afficherlist(list) {
+    personnage.innerHTML = ""
+    list.forEach(element => {
+        personnage.innerHTML = personnage.innerHTML + `
+            <div class="col-lg-4 col-md-4 char">
+                <img src="${element.imageUrl}" alt="">
+                <p>${element.fullName}</p>
+                <p>${element.title}</p>
+            </div>
+        
+        `
+    })
+}
+
+function rechercher() {
+    var input = document.getElementById('search').value
+    var tab_search = []
+
+    $.ajax({
+        url: 'https://thronesapi.com/api/v2/Characters',
+        success: function(data, statuts, response) {
+            // var searchFullName = data.filter(function(element){
+            //     return element.fullName === input
+            // })
+            data.forEach(element => {
+                if (element.fullName.includes(input)) {
+                    tab_search.push(element)
+                }
+                
+            })
+            afficherlist(tab_search);
+        }
+    })
+}
+
+

74b0fde039c628a8d2c65b5ef315f364b93c17bc
Yon Txpmdroflol <jochong27@gmail.com>
2021-10-20 10:47:19 +0200

    shuffle

--- index.js
+++ index.js
@@ -1,36 +1,39 @@
-var personnage = document.getElementsByClassName('personnage')[0]
 
 $(function() {
+    var personnage = document.getElementsByClassName('personnage')[0]
+    var dataGlobal =[]
+
     $.ajax({
         url: 'https://thronesapi.com/api/v2/Characters',
         success: function(data, statuts, response) {
             afficherlist(data)
-            $(".search input").keyup(function() {
-                var input = $("#search").val()
-                // $('.form').submit(function(e) {
-                //     e.preventDefault()
-                    // var input = $("#search").val()
-                    var tab_search = []
-                    // var searchFullName = data.filter(function(element){
-                    //     return element.fullName === input
-                    // })
-                    data.forEach(element => {
-                        if (element.fullName.includes(input)) {
-                            tab_search.push(element)
-                        }
-                    })
-                    afficherlist(tab_search)
-                // })
-            })
-            $("#random").click(function(){
-                var random = Math.floor(Math.random() * (data.length - 0 + 1) + 0)
-                console.log(random)
-            })
-            $("#trier").click(function(){
-                var arrayTri = data.sort(SortArray)
-                afficherlist(arrayTri)
-            })
+            dataGlobal = data
         }
+    })
+    $(".search input").keyup(function() {
+        var input = $("#search").val()
+        // $('.form').submit(function(e) {
+        //     e.preventDefault()
+            // var input = $("#search").val()
+            var tab_search = []
+            // var searchFullName = data.filter(function(element){
+            //     return element.fullName === input
+            // })
+            dataGlobal.forEach(element => {
+                if (element.fullName.includes(input)) {
+                    tab_search.push(element)
+                }
+            })
+            afficherlist(tab_search)
+        // })
+    })
+    $("#random").click(function(){
+        var random = _.shuffle(dataGlobal)
+        afficherlist(random)
+    })
+    $("#trier").click(function(){
+        var arrayTri = dataGlobal.sort(SortArray)
+        afficherlist(arrayTri)
     })
 
     function SortArray(x, y){
@@ -44,9 +47,11 @@ $(function() {
         list.forEach(element => {
             personnage.innerHTML = personnage.innerHTML + `
                 <div class="col-lg-4 col-md-4 char">
-                    <img src="${element.imageUrl}" alt="">
-                    <p>${element.fullName}</p>
-                    <p>${element.title}</p>
+                    <div class="border">
+                        <img src="${element.imageUrl}" alt="">
+                        <p>${element.fullName}</p>
+                        <p>${element.title}</p>
+                    </div>
                 </div>
             
             `
